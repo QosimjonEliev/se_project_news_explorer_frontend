9240d01a47c408b0217ec91395b08a456f42a2a3
QosimjonEliev <128938129+QosimjonEliev@users.noreply.github.com>
2024-01-11 21:08:00 -0500

    Delete src directory

--- src/components/Preloader/Preloader.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Preloader.css";
-
-function Preloader() {
-  return <div className="circle-preloader"></div>;
-}
-
-export default Preloader;
--- src/components/ProtectedRoute/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-
-const ProtectedRoute = ({ isLoggedIn, children, ...props }) => {
-  return <Route {...props}>{isLoggedIn ? children : <Redirect to="/" />}</Route>;
-};
-
-export default ProtectedRoute;
